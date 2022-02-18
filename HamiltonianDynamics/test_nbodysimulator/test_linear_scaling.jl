587d22089364fa4b5ccdd45328238b532168d0b4
Noé Blassel <noe.blassel@gmail.com>
2022-02-18 13:20:32 +0100

    set up nbody benchmark

--- HamiltonianDynamics/test_nbodysimulator/test_linear_scaling.jl
+++ HamiltonianDynamics/test_nbodysimulator/test_linear_scaling.jl
@@ -1,18 +1,31 @@
-using NBodySimulator,BenchmarkTools,Plots
+using NBodySimulator, BenchmarkTools, Plots
 
 include("../utils/ReducedUnits.jl")
 include("../nbodysimulator/SimulateLennardJones.jl")
 include("../nbodysimulator/io.jl")
 
-times=[]
-T=0.5
-ρ=0.5
-steps=5000
+times = []
 
-for N=1:12
-    t=@elapsed sim_lennard_jones_fluid(N,ρ,T,5e-3,1000,VelocityVerlet)
-    push!(times,t)
-    println(N)
+T = 1.0
+ρ = 0.2
+r_c = 3
+
+steps = 5000
+
+Npd_range = 4:15
+n_samps = 10
+
+for N = 1:12
+    t = 0
+    for i = 1:n_samps
+        t += @elapsed sim_lennard_jones_fluid(N, ρ, T, r_c, 5e-3, 10000, VelocityVerlet)
+    end
+    push!(times, t / n_samps)
 end
 
-plot([N^3 for N in 1:12],times,xlabel="N",ylabel="T",title="runtime scaling",label="",markershape=:xcross)
+f = open("nbody_scaling.txt", "w")
+
+for t in times
+    print(f, "$(t) ")
+end
+close(f)
