e91eec4bf3ba64bce343a1669fb45d646881cfe9
Noé Blassel <noe.blassel@gmail.com>
2022-02-18 12:42:28 +0100

    added nbodysimulator pressure

--- HamiltonianDynamics/nbodysimulator/custom_observables.jl
+++ HamiltonianDynamics/nbodysimulator/custom_observables.jl
@@ -4,7 +4,7 @@ LennardJonesParameters
     lj_params = sr.simulation.system.potentials[:lennard_jones]
     σm6 = inv(lj_params.σ2^3)
     ϵ = lj_params.ϵ
-    R2=lj_params.R2
+    R2 = lj_params.R2
     pbc = sr.simulation.boundary_conditions
 
     N = length(sr.simulation.system.bodies)
@@ -16,14 +16,14 @@ LennardJonesParameters
 
         @threads for i = 1:N
             for j = 1:i-1
-                ri = q[:,i]
-                rj = q[:,j]
+                ri = @view q[:, i]
+                rj = @view q[:, j]
                 (_, _, r2) = NBodySimulator.get_interparticle_distance(ri, rj, pbc)
 
                 if r2 < R2
                     rm6 = inv(r2^3)
-                    wij=24ϵ*σm6*(2σm6*rm6^2-rm6)
-                    W_threads[threadid()]+=wij
+                    wij = 24ϵ * σm6 * (2σm6 * rm6^2 - rm6)
+                    W_threads[threadid()] += wij
                 end
 
             end
@@ -34,19 +34,38 @@ LennardJonesParameters
         W = 0
         for i = 1:N
             for j = 1:i-1
-                ri = @view q[:,i]
-                rj = @view q[:,j]
+                ri = @view q[:, i]
+                rj = @view q[:, j]
                 (_, _, r2) = NBodySimulator.get_interparticle_distance(ri, rj, pbc)
 
-                if r2<R2
+                if r2 < R2
                     rm6 = inv(r2^3)
-                    wij=24ϵ*σm6*(2σm6*rm6^2-rm6)
-                    W+=wij
+                    wij = 24ϵ * σm6 * (2σm6 * rm6^2 - rm6)
+                    W += wij
                 end
 
             end
         end
         return W
     end
+
+end
+
+function pressure(sr::NBodySimulator.SimulationResult, time::Real, parallel::Bool = true)
+
+    W = virial(sr, time, parallel)
+    K = kinetic_energy(sr, time)
+    V = nothing
+    if isa(sr.simulation.boundary_conditions, CubicPeriodicBoundaryConditions)
+        V = sr.simulation.boundary_conditions.L^3
+    elseif isa(sr.simulation.boundary_conditions, PeriodicBoundaryConditions)
+        lm1, lM1, lm2, lM2, lm3, lM3 = sr.simulation.boundary_conditions
+        V = (lM1 - lm1) * (lM2 - lm2) * (lM3 - lm3)
+
+    else
+        error("Only periodic boundary conditions are supported")
+    end
+
+    return (2K+W)/3V
 
 end
--- HamiltonianDynamics/test_nbodysimulator/test_virial.jl
+++ HamiltonianDynamics/test_nbodysimulator/test_virial.jl
@@ -4,13 +4,13 @@ using NBodySimulator,Base.Threads
 include("../nbodysimulator/custom_observables.jl")
 include("../nbodysimulator/SimulateLennardJones.jl")
 
-Npd=10
+Npd=6
 ρ=0.2
 T=0.5
 
-res=sim_lennard_jones_fluid(Npd,ρ,T,4.0,5e-3,5000,VelocityVerlet)
+res=sim_lennard_jones_fluid(Npd,ρ,T,4.0,5e-3,100,VelocityVerlet)
 
-@time V1=[pair_virial_lj(res,time) for time in res.solution.t ]
-@time V2=[pair_virial_lj(res,time,false) for time in res.solution.t ]
+@time V1=[pair_virial_lj(res,time) for time in res.solution.t]
+@time V2=[pair_virial_lj(res,time,false) for time in res.solution.t]
 
 @assert all(V1.≈V2)
