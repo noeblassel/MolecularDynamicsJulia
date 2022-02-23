f270e9bfd7b0086a7e6a5da702a30c0c89849f29
Noé Blassel <noe.blassel@gmail.com>
2022-02-23 15:25:30 +0100

    edit langevin test

--- Langevin/test_ou.jl
+++ Langevin/test_ou.jl
@@ -1,15 +1,9 @@
-using Molly, ProgressMeter
+using Plots, Statistics, Profile
 
-include("../molly/custom_simulators.jl")
-include("../molly/custom_observables.jl")
-include("../molly/custom_loggers.jl")
-include("../molly/custom_cutoffs.jl")
-
-include("../utils/ReducedUnits.jl")
-include("../utils/PlaceAtoms.jl")
+include("../molly/MollyExtend.jl")
 
 ρ = 0.2
-T = 0.5
+T = 0.3
 
 r_a = 2.5
 r_c = 4.0
@@ -35,41 +29,40 @@ catch
     global nf = TreeNeighborFinder(nb_matrix = trues(N, N), dist_cutoff = r_c)
 end
 
-loggers = Dict(:velocities => VelocityLogger(Float64, 1))
 
-sys = System(atoms = atoms, coords = coords, velocities = deepcopy(velocities), general_inters = (), box_size = box_size, neighbor_finder = nf, force_units = NoUnits, energy_units = NoUnits, loggers = loggers)
+γ = 1.0
+eq_steps=10000
+samp_steps = 30000
 
-
-γ = 10.0
-n_steps = 10000
 dt = 5e-3
 
 seed = 1234
 
 simulator = LangevinTest(dt = dt, γ = γ, T = T, rseed = seed)
-@time simulate!(sys, simulator, n_steps)
 
-t_range = 0:dt:(n_steps*dt-dt)
-E(t) = exp(-γ * t)
-E_t = E.(t_range)
-V(t) = T * (1 - E(2t))
-V_t = V.(t_range)
+sys = System(atoms = atoms, coords = coords, velocities = deepcopy(velocities), general_inters = (), box_size = box_size, neighbor_finder = nf, force_units = NoUnits, energy_units = NoUnits)
 
-q = zeros(n_steps, 3N)
+@profile simulate!(sys, simulator, eq_steps)
 
-for t in 1:n_steps
+loggers = Dict(:velocities => VelocityLogger(Float64, 1))
+
+sys = System(atoms = sys.atoms, coords = sys.coords, velocities = sys.velocities, general_inters = (), box_size = sys.box_size, neighbor_finder = sys.neighbor_finder, force_units = NoUnits, energy_units = NoUnits,loggers=loggers)
+
+@profile simulate!(sys,simulator,samp_steps)
+
+t_range = 0:dt:(samp_steps*dt-dt)
+
+
+q = zeros(samp_steps, 3N)
+
+for t in 1:samp_steps
     for i in 1:N
         q[t, 3*(i-1)+1:3*(i-1)+3] = sys.loggers[:velocities].velocities[t][i]
     end
 end
 
-
-using Plots, Statistics
-
 E_hat = mean(q, dims = 2)
 V_hat = var(q, dims = 2)
 
-plot(t_range, E_t)
-plot!(t_range, E_hat)
+#ok for mean and variance
 
-#savefig()
--- molly/MollyExtend.jl
+++ molly/MollyExtend.jl
@@ -7,5 +7,8 @@ include("./custom_loggers.jl")
 include("./custom_observables.jl")
 include("./custom_simulators.jl")
 
+include("./sim_nve_lj.jl")
+include("./sim_nvt_lj.jl")
+
 include("../utils/place_atoms.jl")
 include("../utils/reduced_units.jl")
