df8e9d6edd29a3892955b66e9951c62a71bc6514
Noé Blassel <noe.blassel@gmail.com>
2022-02-25 19:24:35 +0100

    fix test

--- Langevin/multiproc.sh
+++ Langevin/multiproc.sh
@@ -5,7 +5,7 @@ Drho=0.0075125
 
 for rhomin in 0.4502 0.45771249999999997 0.465225 0.4727375 0.48025 0.4877625 0.495275 0.5027874999999999 0.5103 0.5178125 0.525325 0.5328375 0.54035 0.5478625 0.555375 0.5628875 0.5704
 do
-
-nohup /libre/blasseln/julia-1.7.2/bin/julia $rhomin $drho $((rhomin+Drho)) > nohup.out &
+rhomax=$((rhomin+Drho))
+nohup /libre/blasseln/julia-1.7.2/bin/julia $rhomin $drho $rhomax > nohup.out &
 
 done
