07ebdfacb7bfaf0955765d16357354c30ea04352
Noé Blassel <noe.blassel@gmail.com>
2022-03-10 23:23:17 +0100

    fix test

--- Langevin/bias_calc.sh
+++ Langevin/bias_calc.sh
@@ -2,7 +2,7 @@
 
 sim=$1
 
-for dt in 7e-4 8e-4 9e-4
+for dt in 1e-3 2e-3 3e-3 4e-3 5e-3
 do
 nohup /libre/blasseln/julia-1.7.2/bin/julia test_bias.jl 1.25 0.25 $dt 500 $sim bias_test.out > nohup$dt.out &
 done
