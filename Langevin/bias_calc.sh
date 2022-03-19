109e68f7fc5f528f4b2b8752938393763e852e8f
Noé Blassel <noe.blassel@gmail.com>
2022-03-19 19:06:39 +0100

    full scale bias test

--- Langevin/bias_calc.sh
+++ Langevin/bias_calc.sh
@@ -2,7 +2,7 @@
 
 sim=$1
 
-for dt in 1e-1 2e-1 3e-1 4e-1 5e-1
+for dt in 1e-1 2e-1 3e-1 4e-1 5e-1 1e-2 2e-2 3e-2 4e-2 5e-2 6e-2 7e-2 8e-2 9e-2
 do
-nohup /libre/blasseln/julia-1.7.2/bin/julia test_bias.jl 1.5 0.3 $dt 20.0 2000.0 3 40000 4.0 $sim bias_test_64.$sim > nohup$dt.out &
+nohup /libre/blasseln/julia-1.7.2/bin/julia --threads=1 test_bias.jl 1.5 0.3 $dt 20.0 2000.0 3 100000 4.0 $sim bias_test_64.$sim > nohup$dt.out &
 done
