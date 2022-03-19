d249fa33461d6b223c829085ea10f0ed0c8ae6ce
Noé Blassel <noe.blassel@gmail.com>
2022-03-19 16:52:46 +0100

    longer bias test

--- Langevin/bias_calc.sh
+++ Langevin/bias_calc.sh
@@ -4,5 +4,5 @@ sim=$1
 
 for dt in 1e-1 2e-1 3e-1 4e-1 5e-1
 do
-nohup /libre/blasseln/julia-1.7.2/bin/julia test_bias.jl 1.5 0.3 $dt 20 500 3 20000 4.0 $sim bias_test_64.$sim > nohup$dt.out &
+nohup /libre/blasseln/julia-1.7.2/bin/julia test_bias.jl 1.5 0.3 $dt 20.0 2000.0 3 40000 4.0 $sim bias_test_64.$sim > nohup$dt.out &
 done
