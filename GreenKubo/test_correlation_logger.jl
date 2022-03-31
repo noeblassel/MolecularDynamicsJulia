30738929f428be536d7adc2a9f9980ecde0a6f76
Noé Blassel <noe.blassel@gmail.com>
2022-03-31 17:56:47 +0200

    small system

--- GreenKubo/test_correlation_logger.jl
+++ GreenKubo/test_correlation_logger.jl
@@ -2,20 +2,20 @@ using Plots
 
 include("../molly/MollyExtend.jl")
 
-Npd = 15
+Npd = 2
 N = Npd^3
 ρ = 1.0
 L = (N / ρ)^(1 // 3)
 T = 0.1
-dt = 5e-3
+dt = 1e-3
 r_c = 2.5
 
-n_steps = 20_000_000
+n_steps = 100_000_000
 
 box_size = SVector(L, L, L)
 inter = LennardJones(cutoff=ShiftedForceCutoff(r_c), nl_only=true, force_units=NoUnits, energy_units=NoUnits)
 
-nf = CellListMapNeighborFinder(nb_matrix=trues(N, N), dist_cutoff=r_c,unit_cell=box_size)
+nf = DistanceNeighborFinder(nb_matrix=trues(N, N), dist_cutoff=r_c)
 coords =place_atoms_on_lattice(Npd, box_size)
 atoms = [Atom(σ=1.0, ϵ=1.0, mass=1.0) for i in 1:N]
 velocities = [reduced_velocity_lj(T, atoms[i].mass) for i in 1:N]
@@ -27,25 +27,18 @@ sim=LangevinSplitting(dt=dt,γ=0.0,T=T,splitting="BABO")
 
 simulate!(sys,sim_eq,20_000)
 
-## color drift force field
-F=normalize(SVector(1.0,0,0))
-ff=zeros(SVector{3,Float64},N)
+R(s::System,neighbors=nothing)=s.velocities[1][1]
 
-    for ix=1:2:length(ff)
-        ff[ix]=F/N
-    end
-
-    for ix=2:2:length(ff)
-        ff[ix]=F/N
-    end
-
-R=MobilityObservable(ff)
-
-## color drift force field
-
-sys.loggers=Dict(:autocorrelation=>AutoCorrelationLogger(R,2000))
+sys.loggers=Dict(:autocorrelation=>AutoCorrelationLogger(R,500))
 simulate!(sys,sim,n_steps)
 f=open("output.txt","w")
-println(f,join(sys.loggers[:atuocorrelation].correlations," "))
-println(f,sys.loggers[:autocorrelation].avg_A)
+println(f,join(sys.loggers[:autocorrelation].correlations," "))
+println(f,sys.loggers[:autocorrelation].avg_A," ",sys.loggers[:autocorrelation].avg_sq_A)
 close(f)
+
+
+dt_range=1:dt:(dt*n_steps)
+scatter(dt_range,sys.loggers[:autocorrelation].correlations,color=:black,markershape=:circle,markersize=0.5,label="",xlabel="t",ylabel="C(t)",size=(1024,768))
+savefig("solid_autocorrelation.png")
+scatter(dt_range,cumsum(sys.loggers[:autocorrelation].correlations),color=:black,markershape=:circle,markersize=0.5,label="",xlabel="t",ylabel="∫C(t)dt",size=(1024,768))
+savefig("int_solid_autocorrelation.png")
