434281f42ef5ff8798f38daf26759a7692ced2b2
Noé Blassel <noe.blassel@gmail.com>
2022-04-03 18:34:43 +0200

    autocorrelation test full scale

--- GreenKubo/test_correlation_logger.jl
+++ GreenKubo/test_correlation_logger.jl
@@ -6,7 +6,7 @@ N = Npd^3
 ρ = 0.7
 L = (N / ρ)^(1 // 3)
 T = 0.5
-dt = 1e-3
+dt = 2e-3
 r_c = 3.0
 
 n_steps = 10_000_000
@@ -27,7 +27,7 @@ simulate!(sys,sim_eq,50_000)
 
 R(s::System,neighbors=nothing)=s.velocities/N
 
-sys.loggers=Dict(:autocorrelation=>AutoCorrelationLoggerVec(N,3,R,16000))
+sys.loggers=Dict(:autocorrelation=>AutoCorrelationLoggerVec(N,3,R,8000))
 
 simulate!(sys,sim,n_steps;log_progress=true,log_every=10000)
 f=open("output_alt.txt","w")
