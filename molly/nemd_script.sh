a100a373f9a8d76d5104b0c7d76d01d67bb795ed
Noé Blassel <noe.blassel@gmail.com>
2022-04-05 16:37:00 +0200

    changed nemd script

--- molly/nemd_script.sh
+++
@@ -1,6 +0,0 @@
-method=$1
-
-for eta in 0.1 0.2 0.3 0.4 0.5
-do
-nohup /libre/blasseln/julia-1.7.2/bin/julia --threads=3 estimate_mobility.jl 2.5 0.7 5e-3 1.0 $eta $1 100.0 5000.0 10 BAOAB 3.0 > nohup.out &
-done
