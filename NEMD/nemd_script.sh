ee8d72d380bfe9a72acf087e11f9a0fc15a88134
Noé Blassel <noe.blassel@gmail.com>
2022-04-06 11:47:55 +0200

    change etas in nemd script

--- NEMD/nemd_script.sh
+++ NEMD/nemd_script.sh
@@ -1,6 +1,6 @@
 method=$1
 
-for eta in 0.05 0.075 0.125 0.15 0.175
+for eta in 0.1 0.2 0.3 0.4 0.5
 do
 nohup /libre/blasseln/julia-1.7.2/bin/julia --threads=3 estimate_mobility.jl 2.5 0.8 5e-3 1.0 $eta $method 100.0 5000000.0 10 BAOAB 3.0 > nohup.out &
 done
