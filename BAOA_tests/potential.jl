7bfb80af7e6475b2ce0f1931ab97942a1cdca04e
Noé Blassel <noe.blassel@gmail.com>
2022-04-12 22:35:02 +0200

    fix tilted double well force calculation

--- BAOA_tests/potential.jl
+++ BAOA_tests/potential.jl
@@ -8,4 +8,4 @@ double_well_potential(q::Float64,L::Float64,a::Float64=1.0,b::Float64=4.0,c::Flo
 minus_d_double_well_potential(q::Float64,L::Float64,a::Float64=1.0,b::Float64=4.0,c::Float64=0.5)=-a*q+b*q*exp(-q^2/(2c^2))/c^2
 
 tilted_double_well_potential(q::Float64,L::Float64,a::Float64=1.0,b::Float64=4.0,c::Float64=0.5,d::Float64=1.0)=a*q^2/2+b*exp(-q^2/(2c^2))+d*q
-minus_d_tilted_double_well_potential(q::Float64,L::Float64,a::Float64=1.0,b::Float64=4.0,c::Float64=0.5,d::Float64=1.0)=-a*q+b*q*exp(-q^2/(2c^2))/c^2+d
+minus_d_tilted_double_well_potential(q::Float64,L::Float64,a::Float64=1.0,b::Float64=4.0,c::Float64=0.5,d::Float64=1.0)=-a*q+b*q*exp(-q^2/(2c^2))/c^2-d
