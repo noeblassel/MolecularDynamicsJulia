71a334797ad9db90177134b52b4f6d47fc35fc97
Noé Blassel <noe.blassel@gmail.com>
2022-04-13 20:48:15 +0200

    lower dts for nemd response

--- NEMD/nemd_script.sh
+++ NEMD/nemd_script.sh
@@ -1,6 +1,6 @@
 method=$1
 
-for eta in 0.1 0.2 0.3 0.4 0.5
+for eta in 0.01 0.02 0.03 0.04 0.05
 do
 nohup /libre/blasseln/julia-1.7.2/bin/julia --threads=3 estimate_mobility.jl 2.5 0.7 5e-3 1.0 $eta $method 100.0 5000000.0 10 BAOAB 3.0 > nohup.out &
 done
