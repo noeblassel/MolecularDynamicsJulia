a32a1ba00a3a6299c1f7d341c732fb705690a677
Noé Blassel <noe.blassel@gmail.com>
2022-04-13 14:47:50 +0200

    fix script

--- Einstein/script.sh
+++ Einstein/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 N=$1
 
-for i in {1..$N}
+for i in $( eval echo {0..$N} )
 do
 nohup /libre/blasseln/julia-1.7.2/bin/julia --threads=4 compute_msd.jl 2.5 0.7 5e-3 1.0 100.0 1000000.0 1000.0 10 BAOAB 3.0 > nohup$i.out &
 done
