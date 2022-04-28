661cac2d8e1a8af3601fd7e61282fb636f4da43e
Noé Blassel <noe.blassel@gmail.com>
2022-04-28 12:17:42 +0200

    add mass option to 2D computation

--- BAOA_tests/test_anisotropic_M.jl
+++ BAOA_tests/test_anisotropic_M.jl
@@ -5,7 +5,7 @@ include("hist_utils.jl")
 include("potential.jl")
 
 
-println("usage: dt N_particles N_integration_steps N_equilibration_steps log_frequency splitting N_bins potential=PERIODIC|QUADRATIC|DOUBLE_WELL|TILTED_DOUBLE_WELL γ")
+println("usage: dt N_particles N_integration_steps N_equilibration_steps log_frequency splitting N_bins potential=PERIODIC|QUADRATIC|DOUBLE_WELL|TILTED_DOUBLE_WELL γ m")
 force_dict=Dict("PERIODIC"=>minus_grad_periodic_potential_2D)#,"QUADRATIC"=>minus_d_quadratic_potential,"DOUBLE_WELL"=>minus_d_double_well_potential,"TILTED_DOUBLE_WELL"=>minus_d_tilted_double_well_potential)
 
 dt=parse(Float64,ARGS[1])
@@ -18,14 +18,14 @@ N_bins=parse(Int64,ARGS[7])
 
 force=force_dict[ARGS[8]]
 γ=parse(Float64,ARGS[9])
-
+m=parse(Float64,ARGS[10])
 ps=zeros(2N)
 qs=zeros(2N)
 
 L=1.0
 qlims = (ARGS[8]=="PERIODIC" ) ? (0.0,L) : (-5.0,5.0) 
-plims= (-5.0,5.0)
-M=[2.0,1.0]
+plims= (-8.0,8.0)
+M=[m,1.0]
 
 bc = (ARGS[8]=="PERIODIC" ) ? PeriodicBoundaryCondition(L) : InfiniteBox()
 sim=LangevinSplitting(dt=dt,γ=γ,T=1.0,splitting=splitting,bc=bc)
