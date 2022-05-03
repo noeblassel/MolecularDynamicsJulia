fa8e4bf14ec7cd42c6d10d9e3fa53604eb8e249a
Noé Blassel <noe.blassel@gmail.com>
2022-05-03 11:03:05 +0200

    MALA rejection rate test

---
+++ improved_MALA/integrators.jl
@@ -0,0 +1,94 @@
+using Molly
+
+mutable struct MALA
+    dt::Real
+    β::Real
+
+    is_metropolis::Bool
+
+    rseed::UInt32
+    rng::AbstractRNG
+
+    n_accepted::Int64
+    n_total::Int64
+end
+
+function MALA(; dt, T, is_metropolis=true, rseed=UInt32(round(time())), rng=MersenneTwister(rseed))
+    β = ustrip(inv(T))
+    MALA(dt, β, is_metropolis, rseed, rng, 0, 0)
+end
+
+function simulate!(sys::System{D}, sim::MALA, n_steps::Integer; parallel::Bool=true) where {D}
+    neighbors = find_neighbors(sys, sys.neighbor_finder; parallel=parallel)
+    dW = zero(sys.coords)
+    candidate_coords = zero(sys.coords)
+    accels = accelerations(sys, neighbors; parallel=parallel)
+    accels_tilde = zero(accels)
+    H = potential_energy(sys, neighbors)
+    H_tilde = zero(H)
+    σ = sqrt(2 * sim.dt)
+
+    reverse_deviation = zero(sys.coords)
+
+    for i = 1:n_steps
+        run_loggers!(sys, neighbors, i)
+        dW = SVector{D}.(eachrow(randn(sim.rng, Float64, (length(sys), D))))
+
+        ## propose candidate position according to Euler-Maruyama scheme ##
+        @. candidate_coords = sys.coords - sim.β * accels * sim.dt + σ * dW
+        candidate_coords = wrap_coords_vec.(candidate_coords, (sys.box_size,))
+
+        ## temporarily swap candidate position in the system's field to compute candidate potential and gradient ##
+        sys.coords, candidate_coords = candidate_coords, sys.coords
+        H_tilde = potential_energy(sys, neighbors)
+        accels_tilde = accelerations(sys, neighbors; parallel=parallel)
+
+        ## compute deviation of previous position relative to average of EM move from candidate position ##
+        reverse_deviation = (sys.coords - sim.β * sim.dt * accels_tilde) - candidate_coords
+        #note sys.coords is actually the candidate position and candidate_coords the original position
+
+        ## compute log of metropolis ratio ##
+        α = -sim.β * (H_tilde - H) # potential part
+        +dot(dW, dW) / 2 # forward transition term
+        -dot(reverse_deviation, reverse_deviation) / (4 * sim.dt) #reverse transition term
+
+        ## accept / reject step ##
+        U = rand(sim.rng)
+        accepted = (sim.is_metropolis) ? (log(U) < α) : (U < (exp_α / (1 + exp_α)))
+
+        #println()
+       # println("H: $(H)   |H_tilde: $(H_tilde)  | α: $(α) | accept : $(accepted)")
+
+        sim.n_total += 1
+
+        if accepted
+            sim.n_accepted += 1
+            neighbors = find_neighbors(sys, sys.neighbor_finder; parallel=parallel) #recompute neighbors
+            accels, accels_tilde = accels_tilde, accels #reuse candidate gradient as new gradient
+            H = H_tilde #reuse candidate potential as new potential
+        else
+            sys.coords, candidate_coords = candidate_coords, sys.coords #swap back original coordinates
+        end
+    end
+
+end
+
+mutable struct ImprovedMALA
+    dt::Real
+    β::Real
+
+    is_metropolis::Bool
+
+    rseed::UInt32
+    rng::AbstractRNG
+
+    n_accepted::Int64
+    n_total::Int64
+end
+
+function ImprovedMALA(; dt, T, is_metropolis=true, rseed=UInt32(round(time())), rng=MersenneTwister(rseed))
+    β = ustrip(inv(T))
+    ImprovedMALA(dt, β, is_metropolis, rseed, rng, 0, 0)
+end
+
+function simulate!(sys::System{D}, sim::ImprovedMALA, n_steps::Integer; parallel::Bool=true) where {D} end
---
+++ improved_MALA/test_rejection_scaling.jl
@@ -0,0 +1,52 @@
+using Molly,LinearAlgebra,Random
+
+include("../molly/MollyExtend.jl")
+include("integrators.jl")
+
+using .MollyExtend
+
+barker=parse(Bool,ARGS[1])
+
+Npd=4
+N=Npd^3
+
+dt=5e-7
+
+
+ρ=0.2
+L = (N / ρ)^(1 // 3)
+box_size=SVector(L,L,L)
+
+atoms=[Atom(ϵ=1.0,mass=1.0,σ=1.0) for i=1:N]
+coords=place_atoms_on_3D_lattice(Npd,box_size)
+velocities=init_velocities(1.0,[a.mass for a=atoms],1.0)
+
+#------------------------   ----------------------------------
+r_c_nf=2.5
+
+if (L > 3 * r_c_nf) && N>900
+    nf = CellListMapNeighborFinder(nb_matrix = trues(N, N), dist_cutoff = r_c_nf, unit_cell = box_size)
+elseif N > 900
+    nf = TreeNeighborFinder(nb_matrix = trues(N, N), dist_cutoff = r_c_nf)
+else
+    nf = DistanceNeighborFinder(nb_matrix = trues(N, N), dist_cutoff = r_c_nf)
+end
+#-----------------------   -----------------------------------
+r_c_inter=2.0
+
+inter = LennardJones(cutoff = ShiftedForceCutoff(r_c_inter), nl_only = true, force_units = NoUnits, energy_units = NoUnits)
+
+sys = System(atoms = atoms, coords = coords, velocities = velocities, pairwise_inters = (inter,), box_size = box_size, neighbor_finder = nf, force_units = NoUnits, energy_units = NoUnits, loggers = Dict{Symbol,Any}())
+
+n_steps_eq=100_000
+sim=MALA(dt=1e-6,T=1.0)
+simulate!(sys,sim,n_steps_eq)
+
+
+n_steps_sim=10_000_000
+for lg_dt in range(-7,-5,20)
+    dt=10^lg_dt
+    sim=MALA(dt=dt,T=1.0)
+    println(dt," ",sim.n_accepted/sim.n_total)
+    flush(stdout)
+end
