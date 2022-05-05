8541e4d698a0d12c58665627a297fd2ab2ca41ad
Noé Blassel <noe.blassel@gmail.com>
2022-05-05 20:31:49 +0200

    lower timesteps

--- improved_MALA/script.sh
+++ improved_MALA/script.sh
@@ -1,6 +1,6 @@
 rule=$1
 proposal=$2
 
-for lg_dt in -4.0 -3.8 -3.6 -3.4 -3.2 -3.0
+for lg_dt in -5.0 -4.8 -4.6 -4.4 -4.2
 do nohup /libre/blasseln/julia-1.7.2/bin/julia --threads=8 compute_diffusion.jl 4 $lg_dt 0.4 1000000 0.75 $rule $proposal>nohup.out&
 done
