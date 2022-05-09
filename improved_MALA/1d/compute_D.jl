d529fad18379ae021f3a181a4134a421d3161700
Noé Blassel <noe.blassel@gmail.com>
2022-05-09 08:06:51 +0200

    recompute D

--- improved_MALA/1d/compute_D.jl
+++ improved_MALA/1d/compute_D.jl
@@ -3,8 +3,8 @@ using Base.Threads
 V(q::Float64)=sin(2π*q)
 grad_V(q::Float64)=2π*cos(2π*q)
 
-η=1e-3
-dq=1e-5
+η=1e-2
+dq=1e-4
 q_range=0.0:dq:1.0
 n=length(q_range)
 ψ=zero(q_range)
@@ -23,15 +23,15 @@ for (i,q) in enumerate(q_range)
     (i%100 ==0) && (println("$i:$n"))
 end
 
-Z_η=0.0
+Z=0.0
 for (ψ1,ψ2) in zip(ψ,ψ[2:end])
-    Z_η+=0.5*(ψ1+ψ2)*dq
+    global Z+=0.5*(ψ1+ψ2)*dq
 end
 
-ψ/=Z_η
+ψ/=Z
 
 D=1.0
 for (ψ1,ψ2,q1,q2) in zip(ψ,ψ[2:end],q_range,q_range[2:end])
-    D-=0.5*dq*(ψ1*grad_V(q1)+ψ2*grad_V(q2))/η
+    global D-=0.5*dq*(ψ1*grad_V(q1)+ψ2*grad_V(q2))/η
 end
 println(D)
