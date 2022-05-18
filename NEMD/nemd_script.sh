e99e03042d7d1402e082686836a6acea498a4b2a
Noé Blassel <noe.blassel@gmail.com>
2022-05-18 12:34:13 +0200

    change eta scale for color drift

--- NEMD/nemd_script.sh
+++ NEMD/nemd_script.sh
@@ -1,6 +1,10 @@
 method=$1
 
-for eta in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
+eta_min=$2
+eta_max=$3
+eta_inc=$4
+
+for eta in `LANG=en_US seq $eta_min $eta_inc $eta_max`
 do
 nohup /libre/blasseln/julia-1.7.2/bin/julia --threads=8 estimate_mobility.jl 1.25 0.6 1e-3 1.0 $eta $method 100.0 5000000.0 10 BAOAB 2.5 > nohup.out &
 done
