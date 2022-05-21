7646526c11e664d177414098e096ac76450c26ac
Noé Blassel <noe.blassel@gmail.com>
2022-05-21 13:30:08 +0200

    add color node

--- NEMD/results/plot_mobility_estimates.jl
+++ NEMD/results/plot_mobility_estimates.jl
@@ -4,7 +4,7 @@ using Plots,LinearAlgebra
 path_orig="/libre/blasseln/MolecularDynamicsJulia/NEMD/*.out"
 path_end="."
 
-for node in [15,16]
+for node in [15,16,20]
   run(`scp clustern$node:$path_orig $path_end`)
 end
 
