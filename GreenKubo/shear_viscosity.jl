86890c1dec97aa03aca524c6ce1b245b9a1df0dc
Noé Blassel <noe.blassel@gmail.com>
2022-07-11 21:19:38 +0200

    multiple forcings in a single simulation

--- GreenKubo/shear_viscosity.jl
+++ GreenKubo/shear_viscosity.jl
@@ -5,8 +5,8 @@ T=0.8
 dt=1e-3
 γ=1.0
 t_eq=100.0
-t_corr=5.0
-Npd=16
+t_corr=10.0
+Npd=10
 splitting="BAOAB"
 r_c=2.5
 output_file="autocorrelations_sv.out"
@@ -33,22 +33,41 @@ n_iter_sim=20000
 sim=LangevinSplitting(dt=dt,temperature=T,friction=γ,splitting="BAOAB",remove_CM_motion=false)
 norm_cst=inv(sqrt(3N))
 
-function R(sys,args...;kwargs ...)
-    N=length(sys)
-    x_velocities=view(reinterpret(reshape,Float64,sys.velocities),1,:)
-    y_coords=view(reinterpret(reshape,Float64,sys.coords),2,:)
-    return dot(x_velocities,sin.(2π*y_coords/L))/N
+sinus_forcing=(y-> sin(2π*y/L))
+pw_constant_forcing=(y -> (y<L/2) ? -1 : 1)
+pw_linear_forcing=(y -> (y<L/2) ? 4*(y-L/4)/L : 4*(3L/4-y)/L)
+
+function shear_conj_response(forcing)
+    function R(sys,args...;kwargs...)
+        N=length(sys)
+        x_velocities=view(reinterpret(reshape,Float64,sys.velocities),1,:)
+        y_coords=view(reinterpret(reshape,Float64,sys.coords),2,:)
+        return dot(x_velocities,forcing.(y_coords))/sqrt(N)
+    end
+    return R
 end
+
+(sin_response,const_response,lin_response)=shear_conj_response.([sinus_forcing,pw_constant_forcing,pw_linear_forcing])
 
 sys=System(atoms=atoms,coords=coords,velocities=velocities,pairwise_inters=(inter,),neighbor_finder=nf,boundary=boundary,energy_units=NoUnits,force_units=NoUnits,k=1.0)
 simulate!(sys,sim,n_steps_eq)
-sys=System(atoms=atoms,coords=sys.coords,velocities=sys.velocities,pairwise_inters=(inter,),neighbor_finder=nf,boundary=boundary,energy_units=NoUnits,force_units=NoUnits,k=1.0,loggers=(autocorrelation=AutoCorrelationLogger(R,Float64,1,l_corr),))
+sys=System(atoms=atoms,coords=sys.coords,velocities=sys.velocities,pairwise_inters=(inter,),neighbor_finder=nf,boundary=boundary,energy_units=NoUnits,force_units=NoUnits,k=1.0,loggers=(sinus=AutoCorrelationLogger(sin_response,Float64,1,l_corr),constant=TimeCorrelationLogger(sin_response,const_response,Float64,Float64,1,l_corr),lin=TimeCorrelationLogger(sin_response,lin_response,Float64,Float64,1,l_corr)))
 
 for i=1:n_iter_sim
 simulate!(sys,sim,n_steps_eq)
-    f=open(output_file,"a")
-    println(f,values(sys.loggers.autocorrelation;normalize=false))
-    println(f,sys.loggers.autocorrelation.n_timesteps)
+    f=open("auto_corr_sinus.jl","w")
+    println(f,"corr=",values(sys.loggers.sinus;normalize=false))
+    println(f,"n_steps=",sys.loggers.sinus.n_timesteps)
+    close(f)
+
+    f=open("auto_corr_const.jl","w")
+    println(f,"corr=",values(sys.loggers.constant;normalize=false))
+    println(f,"n_steps=",sys.loggers.constant.n_timesteps)
+    close(f)
+
+    f=open("auto_corr_lin.jl","w")
+    println(f,"corr=",values(sys.loggers.lin;normalize=false))
+    println(f,"n_steps=",sys.loggers.lin.n_timesteps)
     close(f)
 end
 
