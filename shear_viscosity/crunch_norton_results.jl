5b0bafe851fdf8afa7fbea0a70f62674841f6e77
Noé Blassel <noe.blassel@gmail.com>
2022-07-17 15:05:26 +0200

    crunch norton sv results

---
+++ shear_viscosity/crunch_norton_results.jl
@@ -0,0 +1,47 @@
+using Statistics
+
+function asymptotic_var(v::Vector{Float64})
+    data=copy(v)
+    avg=mean(v)
+    data .-= avg
+    N_steps=floor(Int64,log2(length(data)))
+    data=data[1:2^N_steps]#crop series to largest possible power of two
+    L=1
+    N=length(data)
+    K=N
+    max_var=0.0
+    while K>1000
+        max_var=max(max_var,varm(data,0.0)*N*inv(K))
+        K >>=1
+            data=(data[1:2:end]+data[2:2:end])/2
+        end
+    return max_var
+end
+
+run(`./scp_sv_results.sh`)
+
+ηs=collect(vcat(collect(0.008:0.008:0.072),collect(0.08:0.08:0.8)))
+normalizing_csts=Dict("SINUSOIDAL"=>1/2,"CONSTANT"=>2/π,"LINEAR"=>-4/π^2)
+
+methods=["SINUSOIDAL","CONSTANT","LINEAR"]
+for method in methods
+    output_file="norton_$method.dat"
+    f_output=open(output_file,"w")
+    norm=normalizing_csts[method]
+    println(f_output,"eta norton_response est_normalized_response N_samps  AV_response, AV_normalized_response")
+    println(method)
+    for eta in etas 
+        println("\t",eta)
+        f=open("fourier_response_$(method)_$(eta).out")
+        C=reinterpret(Float64,read(f))
+        N=length(C)
+        m=mean(C)
+        σ2=asymptotic_var(C)
+        println(f_output,join([eta,m,inv(m*norm),N,σ2,σ2/(norm^2*m^4)]," ")) #get other variance by delta method (see comment below)
+        close(f)
+        rm("fourier_response_$(method)_$(eta).out")
+    end
+end
+close(f_output)
+
+#φ(m)=1/(norm*m) => φ'(m)=-1/(norm * m^2)
---
+++ shear_viscosity/scp_sv_results.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+scp clustern15:/libre/blasseln/MolecularDynamicsJulia/NEMD/*.out .
+scp clustern16:/libre/blasseln/MolecularDynamicsJulia/NEMD/*.out .
