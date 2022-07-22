24719dd1981116b6de68e9797656383aed96ec68
Noé Blassel <noe.blassel@gmail.com>
2022-07-22 17:17:56 +0200

    scale up test

--- semaine_roscoff/sample_transitions_gpr.jl
+++ semaine_roscoff/sample_transitions_gpr.jl
@@ -55,7 +55,7 @@ begin
         end
     end
 
-    algo=GenParRepAlgorithm(16,300,300,1,0.1,spawn_replica,branch_replica,get_gr_obs,get_state,output_transition)
+    algo=GenParRepAlgorithm(128,300,300,1,0.1,spawn_replica,branch_replica,get_gr_obs,get_state,output_transition)
     #
     p0=SVector(0.0,0.0)
     q0=rand(centers)
